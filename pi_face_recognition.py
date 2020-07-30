1ba32b1527c282486e88d48088f36220167c706c
Taraszovics Péter <gtarapeti@gmail.com>
2020-07-30 17:15:52 +0200

    added argument parser

--- pi_face_recognition.py
+++ pi_face_recognition.py
@@ -1,1 +1,10 @@
 # python3 pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
+
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--cascade", required=True,
+	help = "path to where the face cascade resides")
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+args = vars(ap.parse_args())
