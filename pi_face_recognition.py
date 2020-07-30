7265c51444991ed85e1e46d7e7042bfda78524a1
Taraszovics Péter <gtarapeti@gmail.com>
2020-07-30 17:54:36 +0200

    drawing rectangle nad name above box

--- pi_face_recognition.py
+++ pi_face_recognition.py
@@ -55,6 +55,18 @@ while True:
 				name = data["names"][i]
 				counts[name] = counts.get(name, 0) + 1
 
+			name = max(counts, key=counts.get)
+	
+		names.append(name)
+
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+			cv2.rectangle(frame, (left, top), (right, bottom),
+				(0, 255, 0), 2)
+			y = top - 15 if top - 15 > 15 else top + 15
+			cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+				0.75, (0, 255, 0), 2)
+
+
 
 
 
