68384f63e26185b71f2d56d3d6c0338f6dbc6f0e
Taraszovics Péter <gtarapeti@gmail.com>
2020-07-30 17:16:43 +0200

    loaded in pickle

--- pi_face_recognition.py
+++ pi_face_recognition.py
@@ -1,6 +1,8 @@
 # python3 pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
 
 import argparse
+import pickle
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--cascade", required=True,
@@ -8,3 +10,5 @@ ap.add_argument("-c", "--cascade", required=True,
 ap.add_argument("-e", "--encodings", required=True,
 	help="path to serialized db of facial encodings")
 args = vars(ap.parse_args())
+
+data = pickle.loads(open(args["encodings"], "rb").read())
