b15be10b450fd9bdf576aa8254ef91e6bc98de8f
Taraszovics Péter <gtarapeti@gmail.com>
2020-08-02 15:54:53 +0200

    added argument parser

---
+++ face_capture.py
@@ -0,0 +1,9 @@
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--cascade", required=True,
+	help="path to cascade.xml")
+ap.add_argument("-o", "--output", required=True,
+	help="path to output dir")
+
+args = vars(ap.parse_args())
